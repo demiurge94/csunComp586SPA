fe6f717df788a180558d30ee081a6656094b316d
demiurge88 <erick.vicencio.760@my.csun.edu>
2021-12-20 18:57:36 -0800

    final commit

--- DemiAPI/Migrations/20211221014712_second.Designer.cs
+++ DemiAPI/Migrations/20211221014712_second.Designer.cs
@@ -4,14 +4,16 @@ using DemiAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DemiAPI.Migrations
 {
     [DbContext(typeof(MetalMusicContext))]
-    partial class MetalMusicContextModelSnapshot : ModelSnapshot
+    [Migration("20211221014712_second")]
+    partial class second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
